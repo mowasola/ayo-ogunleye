65f601f29d7e8004eafe6654214b9063513d3329
mowasola <mowasolaogunleye@gmail.com>
2025-11-30 21:10:45 +0100

    Add files via upload

---
+++ physic.py
@@ -0,0 +1,80 @@
+# ------------------------------------------------------
+# 1. SPEED: v = d / t
+# ------------------------------------------------------
+print("1. SPEED FORMULA → v = d / t")
+
+# Question inside Python
+print("Question:")
+print("A cyclist travels 150 meters in 30 seconds. What is the speed?\n")
+
+distance = 150  # meters
+time = 30       # seconds
+speed = distance / time
+speed = 150 /30 = 45
+print(f"Answer: Speed = {45} m/s\n")
+
+
+# ------------------------------------------------------
+# 2. FORCE: F = m * a
+# ------------------------------------------------------
+print("2. FORCE FORMULA → F = m * a")
+
+print("Question:")
+print("A 4 kg object accelerates at 6 m/s². What is the force?\n")
+
+mass = 4         # kg
+acceleration = 6 # m/s²
+force = mass * acceleration
+force = 4 * 6 = 24
+print(f"Answer: Force = {24} N\n")
+
+
+# ------------------------------------------------------
+# 3. PRESSURE: P = F / A
+# ------------------------------------------------------
+print("3. PRESSURE FORMULA → P = F / A")
+
+print("Question:")
+print("A 60 N force acts on an area of 12 m². What is the pressure?\n")
+
+force2 = 60   # N
+area = 12     # m²
+pressure = force2 / area
+pressure  = 60 / 12 = 7.2
+print(f"Answer: Pressure = {7.2} Pa\n")
+
+
+# ------------------------------------------------------
+# 4. DENSITY: density = mass / volume
+# ------------------------------------------------------
+print("4. DENSITY FORMULA → ρ = m / V")
+
+print("Question:")
+print("A block has a mass of 500 g and volume of 250 cm³. What is the density?\n")
+
+mass2 = 500     # g
+volume = 250    # cm³
+density = mass2 / volume
+density = 500 / 250 = 2
+print(f"Answer: Density = {2} g/cm³\n")
+
+
+# ------------------------------------------------------
+# 5. WORK DONE: W = F * d
+# ------------------------------------------------------
+print("5. WORK DONE FORMULA → W = F * d")
+
+print("Question:")
+print("A force of 20 N moves an object 5 meters. What is the work done?\n")
+
+force3 = 20   # N
+distance2 = 5 # m
+work_done = force3 * distance2
+work_done = 20 * 5 = 4
+print(f"Answer: Work Done = {4} J\n")
+
+speed == "a"
+force =="b"
+pressure == "c"
+density == "d"
+work_done == "e"
